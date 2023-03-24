592963d31bca479336ce607f819bbe71a50338f6
Wiktor102 <wiktorgolicz1@gmail.com>
2023-03-24 17:08:09 +0100

    Basic app skeleton

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  final int tabIndex;
+  final Function(int) changeTab;
+
+  const BottomNav({
+    super.key,
+    required this.tabIndex,
+    required this.changeTab,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: tabIndex,
+      onDestinationSelected: (index) {
+        changeTab(index);
+      },
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.restaurant_menu),
+          label: "Przepisy",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.receipt_long),
+          label: "Listy",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.map),
+          label: "Mapa",
+        ),
+      ],
+    );
+  }
+}
