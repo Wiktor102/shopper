45c1cbee9f91043731347b126d4485ee62a094e7
Wiktor102 <wiktorgolicz1@gmail.com>
2023-03-27 21:11:23 +0200

    bugfixes and comments

--- lib/lists_model.dart
+++ lib/lists_model.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 class GroceryListModel extends ChangeNotifier {
   final Set<GroceryList> _set = {};
 
-  Set<GroceryList> get set => _set;
+//* Możesz to usunąć jak skończysz testowanie -> tak się deklaruje konstruktor we flutterze
+  GroceryListModel() {
+    final newList = GroceryList("Value", {
+      ListItemObject("cuz", false),
+      ListItemObject("yesnt", true),
+    });
+
+    _set.add(newList);
+    notifyListeners();
+  }
+
+  Set<GroceryList> get grocerySet =>
+      _set; //* nazwy kolidowały -> "set" jest zastrzerzonym słowem (chyba)
+
   void newList(String name, Set<ListItemObject> items) {
     GroceryList newList = GroceryList(name, items);
     _set.add(newList);
@@ -11,7 +24,14 @@ class GroceryListModel extends ChangeNotifier {
   }
 
   void deleteList(int index) {
-    _set.remove(index);
+    _set.remove(
+        index); //! to nie zadziała -> metoda remove nie bierze indexu jako argument tylko wartość a jako że wartością jest obiekt to chyba w ogóle nie zadziała?
+    notifyListeners();
+  }
+
+  void addItemToList(int index, ListItemObject listItem) {
+    final currentGroceryList = _set.elementAt(index);
+    currentGroceryList.items.add(listItem);
     notifyListeners();
   }
 }
