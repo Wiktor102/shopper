bc3104dbfa5119414b9549673bd7e2116489f950
Wiktor102 <wiktorgolicz1@gmail.com>
2023-04-02 17:15:34 +0200

    Favorite recipes are saved

--- lib/recipes_model.dart
+++ lib/recipes_model.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 
 class RecipesModel extends ChangeNotifier {
   final List<Recipe> _recipes = [];
+  List<int> _favoriteIds = [];
 
   List<Recipe> get recipes => _recipes;
-  void readJSON() async {
+  Future<void> readJSON() async {
     final jsonString = await rootBundle.loadString('assets/recipes.json');
     final decoded = jsonDecode(jsonString);
 
@@ -34,6 +36,7 @@ class RecipesModel extends ChangeNotifier {
 
       _recipes.add(
         Recipe(
+          id: recipe['id'] as int,
           name: recipe['title'] as String,
           ingredients: ingredients.isNotEmpty ? ingredients : ["null"],
           steps: steps.isNotEmpty ? steps : ["null"],
@@ -45,26 +48,49 @@ class RecipesModel extends ChangeNotifier {
   }
 
   RecipesModel() {
-    readJSON();
+    readJSON().then((_) async {
+      final box = await Hive.openBox("favoriteRecipes");
+      final savedFav = box.get("favorite");
+      if (savedFav == null) return;
+
+      _favoriteIds = savedFav;
+      for (Recipe recipe in _recipes) {
+        if (!_favoriteIds.contains(recipe.id)) continue;
+        recipe.favorite = true;
+      }
+
+      notifyListeners();
+    });
   }
 
   void toggleFavorites(int index) {
     _recipes[index].favorite = !_recipes[index].favorite ? true : false;
+
+    if (_favoriteIds.contains(_recipes[index].id)) {
+      _favoriteIds.remove(_recipes[index].id);
+    } else {
+      _favoriteIds.add(_recipes[index].id);
+    }
+
+    Hive.box("favoriteRecipes").put("favorite", _favoriteIds);
     notifyListeners();
   }
 }
 
 class Recipe {
+  int id;
   String name;
   List<String> ingredients;
   List<String> steps;
   bool favorite;
   bool custom;
 
-  Recipe(
-      {required this.name,
-      required this.ingredients,
-      required this.steps,
-      this.favorite = false,
-      this.custom = false});
+  Recipe({
+    required this.id,
+    required this.name,
+    required this.ingredients,
+    required this.steps,
+    this.favorite = false,
+    this.custom = false,
+  });
 }
