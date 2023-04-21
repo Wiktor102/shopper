ee637650a6dadf5da37e61d9dd90c6bb6293df74
Wiktor102 <wiktorgolicz1@gmail.com>
2023-04-21 20:15:57 +0200

    Categories selector widget

--- lib/recipes.dart
+++ lib/recipes.dart
@@ -99,6 +99,93 @@ class RecipesList extends StatelessWidget {
     required this.createListFromRecipe,
   });
 
+  void createCustomRecipe(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const CreateRecipe(recipe: null),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<RecipesModel>(context);
+    List<TemporaryRecipe> recipes = []; //lista która pokazuje sie na ekranie
+
+    if (provider.loading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    switch (currentTab) {
+      case RecipesTabs.custom:
+        for (int i = 0; i < provider.recipes.length; i++) {
+          final element = provider.recipes.elementAt(i);
+          if (!element.custom) continue;
+          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
+        }
+        break;
+      case RecipesTabs.favorites:
+        for (int i = 0; i < provider.recipes.length; i++) {
+          final element = provider.recipes.elementAt(i);
+          if (!element.favorite) continue;
+          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
+        }
+        break;
+      default:
+        for (int i = 0; i < provider.recipes.length; i++) {
+          final element = provider.recipes.elementAt(i);
+          if (element.custom) continue;
+          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
+        }
+    }
+
+    return Scaffold(
+      body: recipes.isNotEmpty
+          ? Column(
+              children: [
+                currentTab == RecipesTabs.recipes
+                    ? const CategoryBar()
+                    : const Text(""),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: recipes.length,
+                    itemBuilder: (_, index) => RecipeListItem(
+                        recipes.elementAt(index),
+                        currentTab,
+                        createListFromRecipe),
+                  ),
+                ),
+              ],
+            )
+          : Empty(
+              currentTab == RecipesTabs.custom
+                  ? "Nie utworzono jeszcze żadnego przepisu"
+                  : currentTab == RecipesTabs.favorites
+                      ? "Nie polubiono żadnego przepisu"
+                      : "Brak przepisów",
+              currentTab == RecipesTabs.recipes
+                  ? 'assets/empty.png'
+                  : currentTab == RecipesTabs.favorites
+                      ? 'assets/favRecipes.png'
+                      : 'assets/customRecipes.png'),
+      floatingActionButton: currentTab == RecipesTabs.custom
+          ? FloatingActionButton(
+              onPressed: () => createCustomRecipe(context),
+              child: const Icon(Icons.add),
+            )
+          : null,
+    );
+  }
+}
+
+class RecipeListItem extends StatelessWidget {
+  final TemporaryRecipe recipe;
+  final RecipesTabs currentTab;
+  final Function(Recipe, BuildContext) createListFromRecipe;
+
+  const RecipeListItem(this.recipe, this.currentTab, this.createListFromRecipe,
+      {super.key});
+
   Future<bool> promptForBoolean(context, String dialog) async {
     bool result = false;
     await showDialog(
@@ -135,6 +222,7 @@ class RecipesList extends StatelessWidget {
     );
   }
 
+  // custom
   void createCustomRecipe(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -158,136 +246,158 @@ class RecipesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<RecipesModel>(context);
-    List<TemporaryRecipe> recipes = []; //lista która pokazuje sie na ekranie
+    return ListTile(
+      onTap: () => showDetails(context, recipe.trueIndex),
+      title: Text(recipe.recipe.name),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () => {createListFromRecipe(recipe.recipe, context)},
+            icon: const Icon(Icons.playlist_add),
+          ),
+          IconButton(
+            onPressed: () => provider.toggleFavorites(recipe.trueIndex),
+            icon: Icon(
+              Icons.favorite,
+              color: recipe.recipe.favorite ? Colors.red : null,
+            ),
+          ),
+          if (currentTab == RecipesTabs.custom)
+            PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (value) async {
+                if (value == "edit") {
+                  editCustomRecipe(
+                    context,
+                    recipe.recipe,
+                  );
+                }
 
-    if (provider.loading) {
-      return const Center(child: CircularProgressIndicator());
-    }
-
-    switch (currentTab) {
-      case RecipesTabs.custom:
-        for (int i = 0; i < provider.recipes.length; i++) {
-          final element = provider.recipes.elementAt(i);
-          if (!element.custom) continue;
-          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
-        }
-        break;
-      case RecipesTabs.favorites:
-        for (int i = 0; i < provider.recipes.length; i++) {
-          final element = provider.recipes.elementAt(i);
-          if (!element.favorite) continue;
-          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
-        }
-        break;
-      default:
-        for (int i = 0; i < provider.recipes.length; i++) {
-          final element = provider.recipes.elementAt(i);
-          if (element.custom) continue;
-          recipes.add(TemporaryRecipe(recipe: element, trueIndex: i));
-        }
-    }
-    return Scaffold(
-      body: recipes.isNotEmpty
-          ? ListView.builder(
-              itemCount: recipes.length,
-              itemBuilder: (_, index) => ListTile(
-                onTap: () =>
-                    showDetails(context, recipes.elementAt(index).trueIndex),
-                title: Text(recipes[index].recipe.name),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      onPressed: () => {
-                        createListFromRecipe(
-                            recipes.elementAt(index).recipe, context)
-                      },
-                      icon: const Icon(Icons.playlist_add),
-                    ),
-                    IconButton(
-                      onPressed: () => provider
-                          .toggleFavorites(recipes.elementAt(index).trueIndex),
-                      icon: Icon(
-                        Icons.favorite,
-                        color:
-                            recipes[index].recipe.favorite ? Colors.red : null,
+                if (value == "delete") {
+                  final bool res = await promptForBoolean(
+                      context, "Czy na pewno chcesz usunąć ten przepis?");
+                  if (!res) return;
+                  deleteCustomRecipe(
+                    recipe.recipe.id,
+                    provider,
+                  );
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuItem>[
+                PopupMenuItem(
+                  value: "edit",
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(right: 5),
+                        child: Icon(Icons.edit),
                       ),
-                    ),
-                    currentTab == RecipesTabs.custom
-                        ? PopupMenuButton(
-                            icon: const Icon(Icons.more_vert),
-                            onSelected: (value) async {
-                              if (value == "edit") {
-                                editCustomRecipe(
-                                  context,
-                                  recipes[index].recipe,
-                                );
-                              }
-
-                              if (value == "delete") {
-                                final bool res = await promptForBoolean(context,
-                                    "Czy na pewno chcesz usunąć ten przepis?");
-                                if (!res) return;
-                                deleteCustomRecipe(
-                                  recipes[index].recipe.id,
-                                  provider,
-                                );
-                              }
-                            },
-                            itemBuilder: (BuildContext context) =>
-                                <PopupMenuItem>[
-                              PopupMenuItem(
-                                value: "edit",
-                                child: Row(
-                                  children: const [
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 5),
-                                      child: Icon(Icons.edit),
-                                    ),
-                                    Text("Edytuj"),
-                                  ],
-                                ),
-                              ),
-                              PopupMenuItem(
-                                value: "delete",
-                                child: Row(
-                                  children: const [
-                                    Padding(
-                                      padding: EdgeInsets.only(right: 5),
-                                      child:
-                                          Icon(Icons.delete, color: Colors.red),
-                                    ),
-                                    Text(
-                                      "Usuń",
-                                      style: TextStyle(color: Colors.red),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          )
-                        : const Text("")
-                  ],
+                      Text("Edytuj"),
+                    ],
+                  ),
                 ),
-              ),
+                PopupMenuItem(
+                  value: "delete",
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(right: 5),
+                        child: Icon(Icons.delete, color: Colors.red),
+                      ),
+                      Text(
+                        "Usuń",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             )
-          : Empty(
-              currentTab == RecipesTabs.custom
-                  ? "Nie utworzono jeszcze żadnego przepisu"
-                  : currentTab == RecipesTabs.favorites
-                      ? "Nie polubiono żadnego przepisu"
-                      : "Brak przepisów",
-              currentTab == RecipesTabs.recipes
-                  ? 'assets/empty.png'
-                  : currentTab == RecipesTabs.favorites
-                      ? 'assets/favRecipes.png'
-                      : 'assets/customRecipes.png'),
-      floatingActionButton: currentTab == RecipesTabs.custom
-          ? FloatingActionButton(
-              onPressed: () => createCustomRecipe(context),
-              child: const Icon(Icons.add),
-            )
-          : null,
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryBar extends StatelessWidget {
+  const CategoryBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<RecipesModel>(context);
+    final Map<int, String> unselected =
+        provider.getUnselectedCategories().asMap();
+    final Map<int, String> selected = provider.selectedCategories.asMap();
+
+    return Container(
+      height: 50,
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.grey, width: 1.0))),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          ...selected
+              .map((i, element) =>
+                  MapEntry(i, CategoryBarItem(element, i, true)))
+              .values,
+          ...unselected
+              .map((i, element) =>
+                  MapEntry(i, CategoryBarItem(element, i, false)))
+              .values,
+        ],
+      ),
+    );
+  }
+}
+
+class CategoryBarItem extends StatelessWidget {
+  final String title;
+  final int index;
+  final bool selected;
+
+  const CategoryBarItem(this.title, this.index, this.selected, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<RecipesModel>(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: selected ? Colors.green : null,
+        border: Border.all(color: Colors.green),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      margin: const EdgeInsets.only(right: 10),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(100.0),
+        onTap: () {
+          if (!selected) provider.selectCategory(index);
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 12),
+          child: Center(
+            child: Row(
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(color: selected ? Colors.white : null),
+                ),
+                if (selected)
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    iconSize: 20,
+                    visualDensity: VisualDensity.compact,
+                    onPressed: () => provider.unselectCategory(index),
+                    icon: const Icon(Icons.close),
+                    color: selected ? Colors.white : null,
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
